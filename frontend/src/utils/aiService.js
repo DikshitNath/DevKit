48f9e94fc529c1fc7ba84bc0f3ffc325687d3bbd
DikshitNath <dikshitnath36@gmail.com>
2026-03-04 20:55:47 +0530

    fixing aiService

--- frontend/src/utils/aiService.js
+++ frontend/src/utils/aiService.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+const API = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
 export const generateBody = async (description) => {
-  const res = await axios.post('/api/ai/generate-body', { description }, { withCredentials: true })
+  const res = await axios.post(`${API}/api/ai/generate-body`, { description }, { withCredentials: true })
   return res.data.body
 }
 
 export const explainResponse = async (request, response) => {
-  const res = await axios.post('/api/ai/explain-response', { request, response }, { withCredentials: true })
+  const res = await axios.post(`${API}/api/ai/explain-response`, { request, response }, { withCredentials: true })
   return res.data.explanation
 }
